fe9229d638c550f18768d7f5a0abf4388370977f
Shamar Malcolm <malco0253@gmail.com>
2024-11-27 23:40:14 -0500

    Added two buttons, tables, and image

--- world.js
+++ world.js
@@ -1,21 +1,49 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const b_lookup = document.getElementById("lookup");
+  const b_lookup = document.getElementById("lookup_country");
+  const b2_lookup = document.getElementById("lookup_cities");
   const countries = document.getElementById("result");
   const country = document.getElementById("country");
   console.log(b_lookup);
   console.log(countries);
   console.log(country);
-  const Lookup = async () => {
-    let url = await fetch(
-      `http://localhost/info2180-lab5/world.php?country=${country.value}`
+  const Lookup_countries = () => {
+    const req = new XMLHttpRequest();
+    req.onreadystatechange = () => {
+      if (req.readyState === 4 && req.status === 200) {
+        let text = req.responseText;
+        countries.innerHTML = "";
+        countries.innerHTML = text;
+      }
+    };
+
+    req.open(
+      "GET",
+      `http://localhost/info2180-lab5/world.php?country=${country.value.trim()}`
     );
-    console.log(url);
-    let text = await url.text();
-    countries.innerHTML = "";
-    countries.innerHTML = text;
+    req.send();
+  };
+
+  const Lookup_cities = () => {
+    const req = new XMLHttpRequest();
+    req.onreadystatechange = () => {
+      if (req.readyState === 4 && req.status === 200) {
+        let text = req.responseText;
+        countries.innerHTML = "";
+        countries.innerHTML = text;
+      }
+    };
+    req.open(
+      "GET",
+      `http://localhost/info2180-lab5/world.php?country=${country.value.trim()}&lookup=cities`
+    );
+    req.send();
   };
 
   b_lookup.addEventListener("click", () => {
-    Lookup();
+    Lookup_countries();
+  });
+
+  b2_lookup.addEventListener("click", () => {
+    Lookup_cities();
   });
 });
--- world.php
+++ world.php
@@ -11,13 +11,58 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $country = $_GET['country'] ?? '';
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE'%$country%'");
+$lookup = $_GET['lookup'] ?? '';
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$country = htmlspecialchars(filter_var($country, FILTER_SANITIZE_STRING));
+$lookup = htmlspecialchars(filter_var($lookup, FILTER_SANITIZE_STRING));
 
+
+if ($lookup == 'cities') {
+    $stmt = $conn->query("SELECT * FROM countries JOIN cities ON code=cities.country_code WHERE countries.name LIKE '$country'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+    <table>
+        <tr>
+          <th>Name</th>
+          <th>District</th>
+          <th>Population</th>
+        </tr>
+      <tbody>
+        <?php foreach ($results as $row): ?>
+          <tr>
+            <td><?=$row['name']; ?></td>
+            <td><?=$row['district']; ?></td>
+            <td><?=$row['population']; ?></td>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+    <?php
+} 
+else {
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE'%$country%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+  ?>
+  <table>
+      <tr>
+        <th>Name</th>
+        <th>Continet</th>
+        <th>Independence </th>
+        <th>Head of State</th>
+      </tr>
+    <tbody>
+      <?php foreach ($results as $row): ?>
+        <tr>
+          <td><?=$row['name']; ?></td>
+          <td><?=$row['continent']; ?></td>
+          <td><?=$row['independence_year']; ?></td>
+          <td><?=$row['head_of_state']; ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+  <?php
+}
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+
